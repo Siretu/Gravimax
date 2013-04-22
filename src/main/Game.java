0aaeacec3dd50c76d132efd92a2dba51447bd2fb
Siretu <erikihr@gmail.com>
2013-04-22 16:55:59 +0200

    Fixed left/right collision for non-square objects

--- src/main/Game.java
+++ src/main/Game.java
@@ -14,7 +14,8 @@ public class Game extends BasicGame{
 	private final float speed = 4f;
 	private final float jumpSpeed = 10f;
 	private final float friction = 0.88f;
-	private final float gravity = 0.001f;
+//	private final float gravity = 0.001f;
+	private final float gravity = 0.5f;
 	private final float maxSpeed = 20;
 	
 	int tick = 0;
@@ -39,20 +40,6 @@ public class Game extends BasicGame{
 	}
 
 	@Override
-	public void render(GameContainer gc, Graphics g) throws SlickException {
-		g.setBackground(background);
-		
-		// Draw Borders
-		for (GameObject b : objects){
-			if(b != null){
-				b.draw(g);
-			}
-		}
-		
-		p.draw(g);
-	}
-
-	@Override
 	public void init(GameContainer gc) throws SlickException {
 		System.out.println("Initializing");
 		
@@ -68,10 +55,30 @@ public class Game extends BasicGame{
 		objects[3] = new GameObject(new Rectangle(750,50,50,700),Color.yellow,true);
 		objects[4] = p;
 		objects[5] = new GameObject(new Rectangle(500,600,50,50), Color.black);
-		objects[6] = new GameObject(new Rectangle(150,600,90,50),Color.black);
+		objects[6] = new GameObject(new Rectangle(150,560,50,90),Color.black);
 		
 	}
-
+	
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		g.setBackground(background);
+		
+		// Draw Borders
+		for (GameObject b : objects){
+			if(b != null){
+				b.draw(g);
+			}
+		}
+		
+		p.draw(g);
+		
+		g.drawLine(195, 0, 195, 800);
+		g.drawLine(265, 0, 265, 800);
+		g.drawLine(285, 0, 285, 800);
+		g.drawLine(240, 0, 240, 800);
+	}
+	
+	
 	@Override
 	public void update(GameContainer gc, int delta) throws SlickException {
 		tick++;
@@ -145,9 +152,9 @@ public class Game extends BasicGame{
 						yGravity = 0;
 						xGravity = -gravity;
 						collisionObject = obj;
-						dir2 = -1;
+						dir = 1;
 					}
-				} else if(p.getSpeedX() > 0 && p.collideRight(obj) && Math.abs(p.getX() - (obj.getX() + obj.shape.getWidth() * -1)) < 20){ // Player collided with something to the right
+				} else if(p.getSpeedX() > 0 && p.collideRight(obj) && Math.abs(p.getX() - (obj.getX() + obj.shape.getWidth() * -1)) < 50){ // Player collided with something to the right
 					p.setSpeedX(0);
 					if(xGravity > 0){
 						p.setJumping(false);
@@ -165,7 +172,7 @@ public class Game extends BasicGame{
 //					System.out.println("Collided right");
 				} 
 				
-				if(p.getSpeedY() > 0 && p.collideDown(obj) && Math.abs(p.getY() - (obj.getY() + obj.shape.getHeight() * -1)) < 20){ // Player collided with an object below
+				if(p.getSpeedY() > 0 && p.collideDown(obj) && Math.abs(p.getY() - (obj.getY() + (obj.shape.getHeight()/2 + p.shape.getWidth()/2) * -1)) < 20){ // Player collided with an object below
 					collisionObject2 = obj;
 					dir2 = -1;
 					p.setSpeedY(0);
@@ -175,7 +182,7 @@ public class Game extends BasicGame{
 					xGravity = 0;
 					yGravity = gravity;
 //					System.out.println("Collided down");
-				} else if(p.getSpeedY() < 0 && p.collideUp(obj) && Math.abs(p.getY() - (obj.getY() + obj.shape.getHeight() * 1)) < 20){ // Player collided with an object above
+				} else if(p.getSpeedY() < 0 && p.collideUp(obj) && Math.abs(p.getY() - (obj.getY() + (obj.shape.getHeight()/2 + p.shape.getHeight() / 2) * 1)) < 20){ // Player collided with an object above
 					p.setSpeedY(0);
 					if(yGravity < 0){
 						p.setJumping(false);
@@ -200,17 +207,18 @@ public class Game extends BasicGame{
 
 		// Correct the collision so it doesn't go through solid objects but only do it if the distance is not too long (to prevent collision with several sides of one object)
 		if(collisionObject != null){
-//			System.out.println("Object X: " + collisionObject.getX());
-//			System.out.println("Width: " + collisionObject.shape.getWidth());
-//			System.out.println("Dir: " + dir);
-			p.setX(collisionObject.getX() + collisionObject.shape.getWidth() * dir);
-//			System.out.println("Corrected playerX to: " + p.getX() + "/" + p.getY());
+			System.out.println("Object X: " + collisionObject.getX());
+			System.out.println("Width: " + collisionObject.shape.getWidth());
+			System.out.println("Dir: " + dir);
+			System.out.println("playerX : " + p.getX() + "/" + p.getY());
+			p.setX(collisionObject.getX() + (collisionObject.shape.getWidth()/2 + p.shape.getWidth() / 2)* dir);
+			System.out.println("Corrected playerX to: " + p.getX() + "/" + p.getY());
 		}
 		if(collisionObject2 != null){
 //			System.out.println("Object Y: " + collisionObject2.getY());
 //			System.out.println("Height: " + collisionObject2.shape.getHeight());
 //			System.out.println("Dir2: " + dir2);
-			p.setY(collisionObject2.getY() + collisionObject2.shape.getHeight() * dir2);
+			p.setY(collisionObject2.getY() + (collisionObject2.shape.getHeight()/2 + p.shape.getHeight() / 2) * dir2);
 //			System.out.println("Corrected playerY to: " + p.getX() + "/" + p.getY());
 		}
 		
